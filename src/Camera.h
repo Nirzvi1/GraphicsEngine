a1c7fc03985151987162e137587b8b911249dc6b
Guy Stoppi <guys@qualum.com>
2018-12-25 21:40:39 -0800

    Added visibility to Camera's perspective transform

--- src/Camera.h
+++ src/Camera.h
@@ -6,13 +6,25 @@
 
 class Camera : public CoordinateSystem {
 
+
 public:
+  size_t canvasWidth, canvasHeight;
+
+  Camera(size_t width=100, size_t height=100) : CoordinateSystem{}, canvasWidth{width}, canvasHeight{height} {}
+  Camera(Matrix4d m, size_t width=100, size_t height=100) : CoordinateSystem{m}, canvasWidth{width}, canvasHeight{height} {}
+
   Point2D transformAndPerspective(Point3D point, const CoordinateSystem &other) const {
     return perspective(transformIntoSystem(point, other));
   }
 
-  static Point2D perspective(Point3D p) {
-    return Point2D(p[0] / -p[2], p[1] / -p[2]);
+  Point2D perspective(Point3D p) const {
+    Point2D px(p[0] / -p[2], p[1] / -p[2]);
+
+    if (std::abs(px[0]) > canvasWidth / 2 || std::abs(px[1]) > canvasHeight / 2 || p[2] > 0) {
+      return Point2D(-1, -1);
+    } else {
+      return Point2D((px[0] + canvasWidth / 2) / canvasWidth, (px[1] + canvasHeight / 2) / canvasHeight);
+    }
   }
 
 };
